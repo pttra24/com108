3515c06b209e4eb9ae482ff0478be426b46f0da9
Phạm Thanh Trà <122071948+pttra24@users.noreply.github.com>
2023-02-17 15:00:16 +0700

    upload

---
+++ lab78/bai1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+	int i;
+	 for ( i = 0; i <=100 ; i++ ) {
+	 	printf(" %d",i);
+   	    if((i+1)%10==0)
+   	       printf("\n");
+   
+    
+   }
+   return 0;
+   
+}
